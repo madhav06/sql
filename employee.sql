577dff814f6608fa897ae92db11419ee23bb3620
mnandan06 <mnandan06@gmail.com>
2016-10-06 17:26:51 +0530

    15 fields (employee's) has been added to employee.sql

--- employee.sql
+++ employee.sql
@@ -1,12 +1,10 @@
-DESC employee;
-
-INSERT INTO employee(id, name, dob, email)
-VALUES (1901, 'Madhav', '1993-03-01' , 'mnandan06@gmail.com');
-
-SELECT * FROM employee;
 
 
 insert into employee
-values (1902, 'Keshav' , '1990-08-11' , 'nandan55@gmail.com');
+values (1915,'Lilee', 19, 'lilee@gmail.com', 19150.00);
+
 
 select * from employee;
+
+
+
