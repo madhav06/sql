4a6c18df5c86c284cc20ee995edd28dc52411166
mnandan06 <mnandan06@gmail.com>
2016-10-06 12:43:19 +0530

    test.sql is a DATABASE created, used to CREATE TABLE employee.sql

---
+++ employee.sql
@@ -0,0 +1,9 @@
+USE test;
+CREATE TABLE employee
+(
+id int primary key,
+name varchar(30),
+dob datetime,
+email varchar(40)
+);
+DESC employee;
--- sample.sql
+++ sample.sql
@@ -2,3 +2,4 @@ CREATE DATABASE sample;
 
 USE sample;
 
+DROP DATABASE sample;
