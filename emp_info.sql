4f86c0500299acdba62239fa487c3fd37f90ca0e
mnandan06 <mnandan06@gmail.com>
2016-10-06 12:57:49 +0530

    creating table from tables: from employee -> emp_info

---
+++ emp_info.sql
@@ -0,0 +1,7 @@
+DESC employee;
+
+CREATE TABLE emp_info AS
+SELECT id, name
+FROM employee;
+
+DESC emp_info;
--- employee.sql
+++ employee.sql
@@ -1,9 +1,1 @@
-USE test;
-CREATE TABLE employee
-(
-id int primary key,
-name varchar(30),
-dob datetime,
-email varchar(40)
-);
 DESC employee;
